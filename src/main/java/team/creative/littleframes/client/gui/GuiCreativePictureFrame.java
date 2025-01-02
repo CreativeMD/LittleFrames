d48d16b49fd58b3fa47bfa4d796b7da37da3b894
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-01-02 22:34:25 +0100

    Removed url length restriction of 512 (now it is 2048)

--- src/main/java/team/creative/littleframes/client/gui/GuiCreativePictureFrame.java
+++ src/main/java/team/creative/littleframes/client/gui/GuiCreativePictureFrame.java
@@ -175,7 +175,6 @@ public class GuiCreativePictureFrame extends GuiLayer {
         flow = GuiFlow.STACK_Y;
         
         url = new GuiUrlTextfield(save, "url", frame.data.getURIPath());
-        url.setMaxStringLength(512);
         add(url);
         GuiLabel error = new GuiLabel("error").setDefaultColor(ColorUtils.RED);
         if (frame.isClient() && frame.cache != null) {
